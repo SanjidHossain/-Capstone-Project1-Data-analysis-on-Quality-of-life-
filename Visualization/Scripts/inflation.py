5ed1e8ff8fb0ac67611421a4c786e01320371c5f
Sanjid Hossain <58914332+SanjidHossain@users.noreply.github.com>
2023-06-15 20:45:27 +0600

    Add files via upload

---
+++ Visualization/Scripts/inflation.py
@@ -0,0 +1,57 @@
+<<<<<<< HEAD
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+
+def main():
+    webdriver_path = "C:\Program Files (x86)\chromedriver.exe"
+    driver = webdriver.Chrome(webdriver_path)
+    url = "https://tradingeconomics.com/country-list/inflation-rate?continent=world"
+    driver.get(url)
+    
+    table = driver.find_element(By.CLASS_NAME, "table-responsive")
+    rows = table.find_elements(By.TAG_NAME, "tr")
+    
+    data = []
+    for row in rows[1:]:
+        cells = row.find_elements(By.TAG_NAME, "td")
+        row_data = [cell.text for cell in cells]
+        data.append(row_data)
+    
+    df = pd.DataFrame(data)
+    df.to_csv("inflation.csv", index=False)
+    
+    return
+
+if __name__ == "__main__":
+    main()
+=======
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import pandas as pd
+
+def main():
+    webdriver_path = "C:\Program Files (x86)\chromedriver.exe"
+    driver = webdriver.Chrome(webdriver_path)
+    url = "https://tradingeconomics.com/country-list/inflation-rate?continent=world"
+    driver.get(url)
+    
+    table = driver.find_element(By.CLASS_NAME, "table-responsive")
+    rows = table.find_elements(By.TAG_NAME, "tr")
+    
+    data = []
+    for row in rows[1:]:
+        cells = row.find_elements(By.TAG_NAME, "td")
+        row_data = [cell.text for cell in cells]
+        data.append(row_data)
+    
+    df = pd.DataFrame(data)
+    df.to_csv("inflation.csv", index=False)
+    
+    return
+
+if __name__ == "__main__":
+    main()
+>>>>>>> 3dcb8de426180df2f8539dcacad16e5646d5182e
